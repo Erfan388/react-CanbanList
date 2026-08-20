453c645b336733f950a11d18ac102eaee953952c
Erfan388 <cpzero81@gmail.com>
2026-08-20 19:34:43 +0330

    [FIX]

--- src/components/Board/components/BoardToolbar/BoardToolbar.tsx
+++ src/components/Board/components/BoardToolbar/BoardToolbar.tsx
@@ -6,27 +6,34 @@ import MingcuteAddLine from "@/components/icons/MingcuteAddLine.tsx";
 import MingcuteEdit2Line from "@/components/icons/MingcuteEdit2Line.tsx";
 import ListModal from "@/modals/ListModal/ListModal.tsx";
 import {BoardpageContext} from "@/context/board-page-context.ts";
+import BoardModal from "@/modals/BoardModal/BoardModal.tsx";
 
 export default function BoardToolbar(): ReactNode {
     const {board} = use(BoardpageContext)
 
-    const modalRef = useRef<HTMLDialogElement>(null);
+    const ListmodalRef = useRef<HTMLDialogElement>(null);
+    const BoardmodalRef = useRef<HTMLDialogElement>(null);
 
     const handleCreateButtonListClick = (): void => {
-        modalRef.current?.showModal();
+        ListmodalRef.current?.showModal();
+    };
+
+    const handleEditeButtonBoardClick = (): void => {
+        BoardmodalRef.current?.showModal();
     };
 
     return <div className={styles['board-toolbar']}>
         <div className={styles.title}>{board.title}</div>
         <div className={styles.actions}>
 
-            <IconButton>
+            <IconButton onClick={handleEditeButtonBoardClick}>
                 <MingcuteEdit2Line/>
             </IconButton>
             <IconButton onClick={handleCreateButtonListClick}>
                 <MingcuteAddLine/>
             </IconButton>
         </div>
-        <ListModal modalRef={modalRef}/>
+        <ListModal modalRef={ListmodalRef}/>
+        <BoardModal boardId={board.id} modalRef={BoardmodalRef} />
     </div>;
 }
