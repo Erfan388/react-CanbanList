d561210b77e07175f5895958308ef2b8df80f14d
Erfan388 <cpzero81@gmail.com>
2026-08-24 20:10:53 +0330

    [ADD] finish sidebar group

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -5,7 +5,6 @@ import {Link} from "react-router";
 import SideBarItem from "@/components/SideBar/components/SideBarItem/SideBarItem.tsx";
 import ExitIcon from '@iconify-react/iconamoon/exit';
 import SideBarGroups from "@/components/SideBar/components/SideBarGroups/SideBarGroups.tsx";
-// import Home7LineIcon from '@iconify-react/mingcute/home-7-line';
 
 export default function SideBar(): ReactNode {
     return <aside className={styles['side-bar']}>
@@ -20,7 +19,7 @@ export default function SideBar(): ReactNode {
         </nav>
 
         <div className={styles.footer}>
-            <SideBarItem title="Sign Out" color="gray" icon={<ExitIcon height="1em" />} />
+            <SideBarItem title="Sign Out" color="gray" icon={<ExitIcon height="30px" />} />
         </div>
     </aside>
 }
--- src/components/SideBar/components/SideBarGroups/SideBarGroups.tsx
+++ src/components/SideBar/components/SideBarGroups/SideBarGroups.tsx
@@ -1,7 +1,65 @@
-import type {ReactNode} from "react";
+import {type ComponentProps, type ReactNode, useContext} from "react";
 
 import styles from './SideBarGroups.module.css';
+import  SideBarItem from "@/components/SideBar/components/SideBarItem/SideBarItem.tsx";
+import {BoardsContext} from "@/context/boards-context.ts";
+import Home7LineIcon from '@iconify-react/mingcute/home-7-line';
+import MingcuteAddLine from "@/components/icons/MingcuteAddLine.tsx";
+import MingcuteEdit2Line from "@/components/icons/MingcuteEdit2Line.tsx";
 
+
+type SideBarGroup = {
+    title?: string;
+    items: ComponentProps<typeof SideBarItem>[];
+}
 export default function SideBarGroups(): ReactNode {
-    return <div className={styles['side-bar-groups']}></div>;
+    const {boards} = useContext(BoardsContext)
+
+    const groups: SideBarGroup[] = [
+        {
+            items: [
+                {
+                    href: '/',
+                    title: "Home",
+                    color: 'gray',
+                    icon: <Home7LineIcon height="30px"/>
+                }
+            ]
+        },
+        {
+            title: 'System',
+            items: [
+                {
+                    href: '/setting',
+                    title: "setting",
+                    color: 'gray',
+                    icon: <MingcuteEdit2Line />
+                }
+            ]
+        },
+        {
+            title: "Boards",
+            items: boards.map((board) => ({
+                href: `/board/${board.id}`,
+                title: board.title ,
+                color: board.color,
+                icon: <MingcuteAddLine />
+            }))
+        }
+    ]
+
+    return groups.map( (group , groupIndex) => (
+        <div key={groupIndex} className={styles.group}>
+            {group.title && <div className={styles.title}>{group.title}</div>}
+                <ul>
+                    {
+                        group.items.map(item => <li key={item.href}>
+                            <SideBarItem {...item} />
+                        </li>)
+                    }
+                </ul>
+        </div>
+    ))
+
+
 }
