8bb0139b6706ae04642282cdc5f02c309377f168
Erfan388 <cpzero81@gmail.com>
2026-08-28 18:38:43 +0330

    [UPDATE] add persist to sidebar-store

--- src/stores/sideBar-store.ts
+++ src/stores/sideBar-store.ts
@@ -1,11 +1,17 @@
 import {create} from "zustand/react";
+import {persist} from "zustand/middleware";
 
 type SideBarStore = {
     isCollapsed: boolean;
     fold: () => void;
 }
 
-export const useSideBarStore = create<SideBarStore>()((set) => ({
-    isCollapsed: false,
-    fold: () => set((state) => ({isCollapsed: !state.isCollapsed})),
-}));
+export const useSideBarStore = create<SideBarStore>()(
+    persist(
+        (set) => ({
+            isCollapsed: false,
+            fold: () => set((state) => ({isCollapsed: !state.isCollapsed})),
+        }),
+        {name : 'sidebar'}
+    )
+);
